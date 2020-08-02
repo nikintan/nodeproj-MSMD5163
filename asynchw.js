ac3d49978617bd2399901a287ef6ef6c687069fb
nikintan <intanzura@yahoo.com>
2020-08-02 20:30:13 +0800

    asynchw update

--- asynchw.js
+++ asynchw.js
@@ -1,22 +1,28 @@
 var fs = require('fs');
 
+console.log("Begin reading file");
+
 fs.readFile("input.txt", (err,data) => {
 	if (err) throw err;
 		console.log(data.toString());
+		console.log("End reading file");
+		fs.appendFile('input.txt','I am appending a file for async', (err)=> {
+			if (err) throw err; 
+				console.log('End appending file');
+				console.log('Begin re-reading file');
+
+				fs.readFile("input.txt", (err,data) => {
+				if (err) throw err;
+					console.log(data.toString());
+					console.log("Finish re-reading file");
+			});
+		});		
 	});
 
-console.log("Begin reading file");
-console.log("End reading file")
 
-fs.appendFile('input.txt','I am appending a file for async', (err)=> {
-	if (err) throw err; 
-	console.log('End appending file');
-	console.log('Begin re-reading file');
-});
 
-var anotherData = fs.readFile("input.txt", (err,data)=> {
-	if (err) throw err;
-		console.log(anotherData);
-		console.log("Finish re-reading file");
-});
+
+
+
+
 
